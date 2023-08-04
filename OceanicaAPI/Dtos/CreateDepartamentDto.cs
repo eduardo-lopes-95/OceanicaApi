99060814506c523ca654ef5a88bb2b642ccedf7a
Eduardo.Lopes <eduardo.lopes-ext@telecall.com>
2023-08-04 15:02:56 -0300

    Atualizando dtos de departament

--- OceanicaAPI/Dtos/CreateDepartamentDto.cs
+++ OceanicaAPI/Dtos/CreateDepartamentDto.cs
@@ -1,5 +1,9 @@
-﻿namespace OceanicaAPI;
+﻿using OceanicaAPI.Models;
+
+namespace OceanicaAPI;
 
 public class CreateDepartamentDto
 {
+    public int VesselId { get; set; }
+    public string Nome { get; set; }
 }
--- OceanicaAPI/Dtos/UpdateDepartamentDto.cs
+++ OceanicaAPI/Dtos/UpdateDepartamentDto.cs
@@ -1,5 +1,9 @@
-﻿namespace OceanicaAPI;
+﻿using OceanicaAPI.Models;
+
+namespace OceanicaAPI;
 
 public class UpdateDepartamentDto
 {
+    public int Id { get; set; }
+    public string Nome { get; set; }
 }
