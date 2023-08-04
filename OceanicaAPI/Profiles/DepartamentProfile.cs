74050c8f70a5ff48b1b14fd938571fc43323bc57
Eduardo.Lopes <eduardo.lopes-ext@telecall.com>
2023-08-04 14:59:45 -0300

    Criando mapeamento de dto para entidade departament

---
+++ OceanicaAPI/Profiles/DepartamentProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using OceanicaAPI.Models;
+
+namespace OceanicaAPI.Profiles;
+
+public class DepartamentProfile : Profile
+{
+    public DepartamentProfile()
+    {
+        CreateMap<CreateDepartamentDto, Departament>().ReverseMap();
+        CreateMap<UpdateDepartamentDto, Departament>().ReverseMap();
+    }
+}
--- OceanicaAPI/Profiles/VesselProfile.cs
+++ OceanicaAPI/Profiles/VesselProfile.cs
@@ -9,6 +9,5 @@ public class VesselProfile : Profile
     {
         CreateMap<CreateVesselDto, Vessel>().ReverseMap();
         CreateMap<UpdateVesselDto, Vessel>().ReverseMap();
-        CreateMap<ReadVesselDto, Vessel>().ReverseMap();
     }
 }
